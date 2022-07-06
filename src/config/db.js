bb5d53165feff5961aef3b18153732738e05382d
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-06 14:04:21 +0700

    update chats

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,12 @@
 const { Pool } = require('pg')
-const {
-  DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
-} = require('../helper/connectEnv')
-
-const config = {
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  port: DB_PORT,
+const db = new Pool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
   ssl: {
     rejectUnauthorized: false
   }
-}
-
-const db = new Pool(config)
-
+})
 module.exports = db
