ca372aa3aba139779ae9e409c7d285ae0cb5cfc0
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-06 13:55:49 +0700

    update chats

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const Router = express.Router()
 
 const authRoute = require('./auth.routers')
-const users = require('./users.routers')
+const usersRoute = require('./users.routers')
 const chatsMesagge = require('./chats.routers')
 
-Router.use('/auth', authRoute).use('/', users).use('/chats', chatsMesagge)
+Router.use('/auth', authRoute).use('/', usersRoute).use('/chats', chatsMesagge)
 module.exports = Router
