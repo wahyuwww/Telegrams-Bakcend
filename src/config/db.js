3629bad5a75db1de8701a0616ec44994372c09bb
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-19 22:42:08 +0700

    make it better

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,7 @@
 const { Pool } = require('pg')
 const {
-  NODE_ENV,
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_PORT
-} = require('../utils/env')
+  DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+} = require('../helper/connectEnv')
 
 const config = {
   host: DB_HOST,
@@ -14,22 +9,11 @@ const config = {
   password: DB_PASSWORD,
   database: DB_NAME,
   port: DB_PORT
-}
-
-if (NODE_ENV === 'production') {
-  config.ssl = {
-    rejectUnauthorized: false
-  }
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
 }
 
 const db = new Pool(config)
-
-db.connect((err) => {
-  if (err) {
-    console.log(err.message)
-    process.exit(1)
-  }
-  console.log('Database connected successfully.')
-})
 
 module.exports = db
