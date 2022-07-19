1b666907cf6c300cdfeea3faf3915dce8f6fa475
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-19 13:34:32 +0700

    make it better

--- src/helper/auth.js
+++ src/helper/auth.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken')
 const generateToken = (payload) => {
   const verify = {
-    expiresIn: 60 * 90,
-    issuer: 'hiring donk'
+    expiresIn: 60 * 90
   }
   const token = jwt.sign(payload, process.env.SECRET_KEY, verify)
   return token
@@ -10,8 +9,7 @@ const generateToken = (payload) => {
 const generateRefreshToken = (payload) => {
   return new Promise((resolve, reject) => {
     const verify = {
-      expiresIn: 60 * 60,
-      issuer: 'hiring donk'
+      expiresIn: 60 * 60
     }
     jwt.sign(payload, process.env.SECRET_KEY, verify, (err, token) => {
       if (!err) {
